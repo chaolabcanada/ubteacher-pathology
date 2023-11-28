0f639199e5259cff870ba6f1519a0803e192a2a1
bendmartin <ben.martin@mail.utoronto.ca>
2023-11-27 23:06:39 -0500

    registration works without splitting

--- train_net2.py
+++ train_net2.py
@@ -1,7 +1,10 @@
-from ubteacher.utils.train2_utils import find_anno_dir, find_img_dir, select_annotypes
+from ubteacher.utils.train2_utils import find_dirs, select_annotypes, find_file, ParseFromQuPath, get_scaling
 from ubteacher import add_ubteacher_config
 from detectron2.config import get_cfg
 from detectron2.engine import default_argument_parser, default_setup, launch
+import os
+import glob
+from pathlib import Path
 
 def setup(args):
     """
@@ -19,14 +22,36 @@ def setup(args):
 def main(args):
     
     cfg = setup(args)
-    anno_dir = find_anno_dir(cfg.ANNO_DIR)
-    img_dir = find_img_dir(cfg.IMG_DIR)
+    img_dirs, anno_dirs = find_dirs(cfg.ANNO_DIR, cfg.IMG_DIR)
     try:
         classes = cfg.DATASET.CLASSES
     except:
-        classes = select_annotypes(anno_dir)
+        classes = select_annotypes(anno_dirs)
     
-    #keep in mind, img_dir will be a list of all the img dirs
+# accumulate dataset_dicts for registration
+
+    dataset_dicts = []
+    image_formats = cfg.FMT.split(',')
+    for anno_dir, img_dir in zip(anno_dirs, img_dirs):
+        for json_file in glob.glob(os.path.join(anno_dir, "*.json")):
+            id = os.path.basename(json_file).split('.')[0]
+            try:
+                original_img_file = find_file(Path(anno_dir).parent, id, cfg.FMT)
+                img_file = os.path.join(img_dir, id + '.npy')
+                base_dim, target_dim = get_scaling(original_img_file, img_file)
+                dataset_dict = ParseFromQuPath(anno_dir, 
+                                            img_dir, 
+                                            base_dim, 
+                                            target_dim, 
+                                            classes
+                                            ).get_coco_format(json_file)
+            except:
+                print(f"Error parsing {json_file}")
+            dataset_dicts.append(dataset_dict)
+            
+# split dataset_dicts into train and val
+
+
     
 if __name__ == "__main__":
     args = default_argument_parser().parse_args()
