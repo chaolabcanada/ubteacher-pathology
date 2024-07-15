301a036ac82591aaa72e61164dfa85d4253f511d
bendmartin <ben.martin@mail.utoronto.ca>
2024-07-15 12:11:08 -0400

    clean up

--- ubteacher_inference_new.py
+++ ubteacher_inference_new.py
@@ -1,50 +1,36 @@
-
+# TODO Make an eval script to output COCO style APs and ARs
 
 # Imports
-import os
-import shutil
-from typing import *
-from time import perf_counter
-
-import matplotlib.pyplot as plt
-import matplotlib.patches as patches
-import numpy as np
-import tifffile as tf
-import torch
-import json
-import glob
-import itertools
 import argparse
-import copy
-import cv2
-import shutil
-import joblib
-
-import sys
+import glob
+import json
+import os
 from pathlib import Path
-import openslide
-
+import shutil
+import sys
 # Hacky way to resolve project paths
 sys.path.append(str(Path(os.getcwd()).parents[0]))
 sys.path.append(str(Path(os.getcwd()).parents[1]))
+from time import perf_counter
+from typing import *
 
-from PIL import Image
-from shapely.geometry import Polygon
-from ubteacher.modeling.meta_arch.rcnn import TwoStagePseudoLabGeneralizedRCNN # required to load model
-from detectron2.engine import DefaultPredictor
+import cv2
+import joblib
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import numpy as np
+import torch
+
+from detectron2.config import get_cfg
+from detectron2.checkpoint import DetectionCheckpointer
+
 from ubteacher.modeling.meta_arch.ts_ensemble import EnsembleTSModel
 from ubteacher.engine.trainer import UBRCNNTeacherTrainer
 from ubteacher.config import add_ubteacher_config
 from ubteacher.utils.train2_utils import (register_dataset, channel_last,
                                           ParseUnlabeled, merge_bboxes)
 
-from tiatoolbox.models.engine.nucleus_instance_segmentor import NucleusInstanceSegmentor
-from detectron2.config import get_cfg
-from detectron2.checkpoint import DetectionCheckpointer
-from detectron2.utils.visualizer import Visualizer
-from detectron2.data import DatasetCatalog, MetadataCatalog
-
-## TODO: Cat map support for merged boxes
+## TODO Cat map support for merged boxes
 
 def custom_visualizer(img_id, 
                       img, 
@@ -59,7 +45,6 @@ def custom_visualizer(img_id,
     fig,ax = plt.subplots(1)
     # Display the image
     ax.imshow(img)
-    
     # Loop over instances and draw boxes
     if merged_bboxes:
         for i in range(len(instances)):
@@ -83,7 +68,7 @@ def custom_visualizer(img_id,
                 rect = patches.Rectangle((x1,y1),w,h,linewidth=1,edgecolor='r',facecolor='none')
                 ax.add_patch(rect)
                 ax.text(x1, y1, cat, fontsize=9, color='r')
-                ax.text(x1, y1 + img.shape[1]/20, str(round(instances[i]['score'], 2)), fontsize=7, color='r')
+                ax.text(x1, y1 + img.shape[1]/20, str(round(instances[i]['score'], 2)), fontsize=7, color='r')   
     # Loop over ground truth instances and draw boxes
     if gt_instances is not None:
         for i in range(len(gt_instances[img_id])):
@@ -124,94 +109,13 @@ def parse_gt(gt_dir):
         gt_dict.update({img_id: each_gt})
     return gt_dict
 
-def filter_intensity(img, instance_dicts, filter_count, intensity_thresh = 30):
-    for i in range(len(instance_dicts)):
-        x1, y1, x2, y2 = instance_dicts[i]['bbox']
-        img_crop = img[int(y1):int(y2), int(x1):int(x2)]
-        # Remove instances that are mostly white (background)
-        if np.mean(img_crop) < np.percentile(img, intensity_thresh):
-            instance_dicts[i]['score'] = 0
-    return instance_dicts, filter_count
-
-def nuc_seg(img, instance_dicts, wsi_path, filter_count, size = 50, nuc_th = 0.1):
-    """
-    Perform nuclear segmentation on an image.
-    """
-    ratios = []
-    for i in range(len(instance_dicts)):
-        img_id = wsi_path.split('/')[-1].split('.')[0]
-        nuc_parent = os.path.join(args.output_dir, 'nuc_masks')
-        nuc_path = os.path.join(args.output_dir, 'nuc_masks', img_id + '.png')
-        out_dir = os.path.join(args.output_dir, 'nuc_seg', img_id)
-        
-        x1, y1, x2, y2 = instance_dicts[i]['bbox']
-        # get size of bbox
-        w = x2 - x1
-        h = y2 - y1
-        if w < size * 2 or h < size * 2:
-            nsize = int(min(w, h) / 2)
-        else:
-            nsize = int(size)
-        # get center bbox region
-        xc = int((x1 + x2) / 2)
-        yc= int((y1 + y2) / 2)
-        base_mask = np.zeros(img.shape[:2], dtype = "uint8")
-        base_mask[yc - nsize:yc + nsize, xc - nsize:xc + nsize] = 1
-        mask_vis = base_mask * 255
-        if not os.path.exists(os.path.join(args.output_dir, 'nuc_masks')):
-            os.makedirs(os.path.join(args.output_dir, 'nuc_masks'))
-        cv2.imwrite(nuc_path, base_mask)
-        
-        #TODO: Fix this so I can debug the mask issue
-        plt.imshow(img)
-        plt.imshow(mask_vis, alpha=0.5)
-        plt.savefig(os.path.join(nuc_parent, f'{img_id}_{str(i)}_maskvis.png'))
-        
-        inst_segmentor = NucleusInstanceSegmentor(
-            pretrained_model="hovernet_fast-pannuke",
-            batch_size=16,
-            auto_generate_mask=False,
-            verbose=False,
-        )
-        
-        if os.path.exists(out_dir):
-            shutil.rmtree(out_dir)
-            
-        wsi_output = inst_segmentor.predict(
-            [wsi_path],
-            masks=[nuc_path],
-            save_dir=out_dir,
-            mode="wsi",
-            on_gpu=True,
-            crash_on_exception=True,            
-        )
-        
-        neoplastic = []
-        all = []
-        
-        wsi_pred = joblib.load(f"{wsi_output[0][1]}.dat")
-        nuc_id_list = list(wsi_pred.keys())
-        for n in nuc_id_list:
-            if wsi_pred[n]["type"] == 0:
-                neoplastic.append(n)
-        if len(nuc_id_list) < 10:
-            instance_dicts[i]['score'] = 0
-            os.remove(nuc_path)
-            filter_count += 1
-        elif len(neoplastic)/len(nuc_id_list) < nuc_th:
-            instance_dicts[i]['score'] = 0
-            filter_count += 1
-            os.remove(nuc_path)
-        if not len(nuc_id_list) == 0:
-            ratios.append(len(neoplastic)/len(nuc_id_list))
-        
-    return instance_dicts, filter_count, ratios
     
 def qupath_coordspace(instance_dicts, wsi_path, img, tissue_crop = True):
     """
     Convert preds to QuPath coordinate space.
     """ 
     ## TODO: Support tisue crop
+    # TODO Use one package to load wsi and get properties, don't mix openslide and tifffile
     # get wsi dimensions
     slide = openslide.OpenSlide(wsi_path)
     mppx = float(slide.properties['openslide.mpp-x'])
@@ -284,9 +188,9 @@ def save_annos(qupath_dicts, threshold):
     return
 
 if __name__ == "__main__":
-    # Necessary
+
     parser = argparse.ArgumentParser(
-        description="Predict boxes or masks using a trained UBTeacher2 model. \
+        description="Predict boxes or masks using a trained ubteacher-v2 model. \
                     By default predicts boxes; \
                     use '--mask' to predict masks instead."
     )
@@ -311,15 +215,7 @@ if __name__ == "__main__":
     )
     
     # Optional
-    
-    parser.add_argument(
-        "-wsi",
-        "--wsi_dir",
-        metavar="WSI_DIRECTORY",
-        type=str,
-        help="path to WSI directory for nuc. seg.",
-    )
-    
+      
     parser.add_argument(
         "-v",
         "--validation",
@@ -327,7 +223,6 @@ if __name__ == "__main__":
         help="validation mode",
         default = False
     )
-    
     parser.add_argument(
         "-gt",
         "--ground_truth",
@@ -398,7 +293,6 @@ if __name__ == "__main__":
     cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = threshold
     
     # Create output folder
-    
     if not os.path.exists(args.output_dir):
         os.makedirs(args.output_dir)
     
@@ -410,8 +304,7 @@ if __name__ == "__main__":
         cat_map = None
         print("No category map specified. Using numerical classes.")
         
-    # If validation mode, get dataseed to perform inference on validation set
-        
+    # If validation mode, get dataseed to perform inference on validation set      
     if val_mode:
         gt_tissue = parse_gt(gt_dir)
     
@@ -433,18 +326,17 @@ if __name__ == "__main__":
         each_dict = ParseUnlabeled(img_file).get_unlabeled_coco(img_file)
         inf_dicts.append(each_dict)
     
-    # Load model
+    # Assemble student-teacher model, load checkpoint
     student_model = UBRCNNTeacherTrainer.build_model(cfg)
     teacher_model = UBRCNNTeacherTrainer.build_model(cfg)
     model = EnsembleTSModel(teacher_model, student_model)
     model.eval()
-    used_model = model.modelStudent
-
+    inference_model = model.modelTeacher # Use the teacher model for inference
     checkpointer = DetectionCheckpointer(model) 
     checkpointer.load(cfg.MODEL.WEIGHTS)   
 
+    # Perform inference
     global_tick = perf_counter()
-    # Convert to batched inputs and perform inference
     filter_count = 0
     all_ratios = {}
     for d in inf_dicts:
@@ -456,7 +348,7 @@ if __name__ == "__main__":
         inputs = [{"image": im, "height": im.shape[1], "width": im.shape[2]}]
         
         with torch.no_grad():
-            outputs = used_model(inputs)
+            outputs = inference_model(inputs)
             instances = outputs[0]["instances"].to("cpu")
             instance_dicts = []
             for i in range(len(instances)):
@@ -464,8 +356,6 @@ if __name__ == "__main__":
                                        'bbox': instances[i].pred_boxes.tensor.numpy()[0].tolist(),
                                        'score': instances[i].scores.numpy()[0]})
             final_anno = instance_dicts
-            #filtered, filter_count = filter_intensity(raw_img, instance_dicts, filter_count)
-            # try merging first
             #merged = merge_bboxes(instance_dicts, threshold)
             #final_anno, filter_count, ratios = nuc_seg(raw_img, instance_dicts,
             #                                         wsi_path, filter_count)
@@ -488,17 +378,5 @@ if __name__ == "__main__":
             print(f"Processed {img_id} in {round(toc - tick, 2)}s")
             print(f"Filtered {filter_count} instances.")
     print(f"Total time: {perf_counter() - global_tick} for {len(inf_dicts)} images")
-    print(f"avg. {round((perf_counter() - global_tick)/len(inf_dicts), 2)}s per image)")
-      
-    
-
-  
-    
-    
-     
-        
-    
-    
-    
-    
-    
+    print(f"Average time: {round((perf_counter() - global_tick)/len(inf_dicts), 2)}s per image)")
+   
