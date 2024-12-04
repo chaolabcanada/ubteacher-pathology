55f466265886ded146f012edca8d31b222eb464a
bendmartin <ben.martin@mail.utoronto.ca>
2024-12-04 12:26:31 -0500

    sync

--- ub_lesion_finder.py
+++ ub_lesion_finder.py
@@ -105,7 +105,7 @@ class DatasetHelper:
                     tissue_dataset.append(entry)
             else:
                 if not file.name.startswith('.') and file.name.endswith(self.compatible_formats):
-                    corr_json = os.path.join(json_dir, f"pred_{file.name.split('.')[0]}.json")
+                    corr_json = os.path.join(json_dir, f"pred_{file.name.split('.')[0]}.json") #TODO: is it always pred_ ??
                     if not os.path.exists(corr_json):
                         print(f"Skipping {file.name} as corresponding json file does not exist.")
                         continue
@@ -558,7 +558,7 @@ if __name__ == "__main__":
     #    cat_map = cfg.CATEGORICAL_MAP[0]
     #except IndexError or AttributeError:
     #    print("'CATEGORICAL_MAP' was not loaded from config! Please check!")
-    cat_map = {"neoplastic" : 0}
+    cat_map = {"neoplastic" : 0, "other": 1} # This is hardcoded TODO
     try:
         detection_mode = cfg.DETECTION_MODE
     except AttributeError:
@@ -622,7 +622,7 @@ if __name__ == "__main__":
             ens_model = EnsembleTSModel(teacher_model, student_model)
             checkpointer = DetectionCheckpointer(ens_model)
             checkpointer.load(cfg.MODEL.WEIGHTS)
-            model = ens_model.modelTeacher ## tbd
+            model = ens_model.modelStudent ## tbd
             model.eval()
         else: # For detectron2 models
             model = DefaultTrainer.build_model(cfg)
